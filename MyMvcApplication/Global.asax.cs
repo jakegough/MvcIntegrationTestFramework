ad2a6a47a72249df3cba64d84be700442dec6df8
Iain Ballard <Iain.Ballard@gocompare.com>
2017-03-31 15:33:14 +0100

    Removing scripts, not needed for test framework

--- MyMvcApplication/Global.asax.cs
+++ MyMvcApplication/Global.asax.cs
@@ -4,9 +4,6 @@ using System.Web.Routing;
 
 namespace MyMvcApplication
 {
-    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-    // visit http://go.microsoft.com/?LinkId=9394801
-
     public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
